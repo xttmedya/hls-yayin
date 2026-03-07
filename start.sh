52ace4ecf2a0f6f1036fe2d985345d3e4e4caa5e
xttmedya <xttmedya@gmail.com>
2026-03-07 23:56:00 +0300

    Yeni degisiklikler eklendi

--- start.sh
+++ start.sh
@@ -3,5 +3,9 @@
 LOG_FILE="/app/play.log"
 
 echo "Python playlist oynatıcı başlatılıyor..."
-# Python foreground’da çalışacak
-python3 /app/play.py > "$LOG_FILE" 2>&1
+# Play.py'i background’da çalıştır
+nohup python3 /app/play.py > "$LOG_FILE" 2>&1 &
+
+echo "HTTP server başlatılıyor..."
+# HTTP server foreground’da, Render container için
+exec python3 -m http.server 8080 --directory /app/public
